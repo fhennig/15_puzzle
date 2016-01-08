35736236f6a008b497d6417d4a31d22f27199a1b
Felix Hennig <3hennig@informatik.uni-hamburg.de>
2016-01-08 14:08:14 +0100

    refactoring, testing, fixing

--- src/puzzle.py
+++ src/puzzle.py
@@ -8,15 +8,11 @@ from my_logging import log
 
 
     
-### utility ###
+### utility ### TODO potenziell auslagern
 
 def manhattan_dist(c1, c2):
     assert len(c1) == len(c2), "Coordinates must be of same dimension"
     return sum(abs(np.array(c1) - c2))
-    
-
-def rotate(l, n=1):
-    return l[n:] + l[:n]
 
 
 def product(xs):
@@ -32,27 +28,10 @@ def array_swap(array, p1, p2):
     a_new.itemset(p2, v1)
     a_new.itemset(p1, v2)
     return a_new
+    
 
-
-def on_field(shape, coord):
-    upper = all((np.array(coord) < np.array(shape)).flat)
-    lower = all((np.array(coord) >= np.array(origin(len(shape)))).flat)
-    return upper and lower
-
-
-def puzzle_from_shape(shape):
-    l = rotate(list(range(product(shape))))
-    return from_list(l, shape)
-
-
-def empty_position(p):
-    return get_position(p, 0)
-
-
-def get_position(p, element):
-    res = np.transpose(np.array(np.where(p == element)))
-    assert product(res.shape) != 0, "Element {} not in Puzzle {}".format(element, p)
-    return tuple(res[0])
+def rotate(l, n=1):
+    return l[n:] + l[:n]
 
 
 def coord_to_index(coord, shape):
@@ -71,21 +50,78 @@ def from_list(l, shape):
     return np.fromfunction(np.vectorize(f), shape, dtype=int)
 
 
+### puzzle ###
+
+def on_field(shape, coord):
+    upper = all((np.array(coord) < np.array(shape)).flat)
+    lower = all((np.array(coord) >= np.array(origin(len(shape)))).flat)
+    return upper and lower
+
+
+def puzzle_from_shape(shape):
+    l = rotate(list(range(product(shape))))
+    return from_list(l, shape)
+
+
+def empty_position(p):
+    return get_position(p, 0)
+
+
+def empty_position_target(p):
+    return tuple(np.array(p.shape) - 1)
+
+
+def get_position(p, element):
+    res = np.transpose(np.array(np.where(p == element)))
+    assert product(res.shape) != 0, "Element {} not in Puzzle {}".format(element, p)
+    return tuple(res[0])
+
+
+### solvability ###
+
 def solved(p):
     """Gibt zurück wie das Puzzle gelöst aussehen müsste"""
     return from_list(rotate(sorted(p.flat)), p.shape)
 
 
-def solvable(p):
-    """Gibt an ob das Puzzle lösbar ist."""
-    p1 = parity(p.flat, solved(p).flat)
-    p2 = (-1)**u.manhattan_distance(self.n() - 1, self.n() -1, *self.empty_position())
-    return p1 == p2
-
-
 def is_solved(p):
     """Gibt an ob das Puzzle gelöst ist"""
     return all((p == solved(p)).flat)
+
+
+def solvable(p):
+    """Gibt an ob das Puzzle lösbar ist."""
+    p1 = parity(p.flat, solved(p).flat)
+    p2 = (-1)**manhattan_dist(empty_position(p), empty_position_target(p))
+    return p1 == p2
+
+
+def cycle(perm, init):
+    """perm ist eine bijektion als Dictionary, init ist ein Element
+    aus dem Bildraum. Gibt den Zyklus der Permutation zurück, der init enthält"""
+    cycle = [init]
+    while True:
+        next = perm[cycle[-1]]
+        if next == init:
+            break
+        cycle.append(next)
+    return cycle
+
+
+def parity(perm1, perm2):
+    """perm1, perm2 Listen mit identischen Elementen aber (ggf)
+    unterschiedlicher Reihenfolge der Elemente.
+    Interpretiert perm2 als eine Permutation von perm1 und gibt
+    die Parität dieser Permutation zurück."""
+    perm = dict(zip(perm1, perm2))
+    cycles = []
+    items = perm1[:]
+    while len(items) > 0:
+        c = cycle(perm, items[0])
+        cycles.append(c)
+        items = [i for i in items if i not in c]
+    transpositions = sum([len(c) - 1 for c in cycles])
+    return (-1)**transpositions
 
 
 ### actions ###
@@ -126,7 +162,6 @@ def apply_actions(p, acts):
     for a in acts:
         p = apply_action(p, a)
     return p
-        
 
 
 def reverse_action(a): # ??
@@ -139,36 +174,6 @@ def shuffle(p, n=1000):
         action = random.choice(possible_actions(p))
         p = apply_action(p, action)
     return p
-
-
-### solvability ###
-
-def cycle(perm, init):
-    """perm ist eine bijektion als Dictionary, init ist ein Element
-    aus dem Bildraum. Gibt den Zyklus der Permutation zurück, der init enthält"""
-    cycle = [init]
-    while True:
-        next = perm[cycle[-1]]
-        if next == init:
-            break
-        cycle.append(next)
-    return cycle
-
-
-def parity(perm1, perm2):
-    """perm1, perm2 Listen mit identischen Elementen aber (ggf)
-    unterschiedlicher Reihenfolge der Elemente.
-    Interpretiert perm2 als eine Permutation von perm1 und gibt
-    die Parität dieser Permutation zurück."""
-    perm = dict(zip(perm1, perm2))
-    cycles = []
-    items = perm1[:]
-    while items:
-        c = cycle(perm, items[0])
-        cycles.append(c)
-        items = [i for i in items if i not in c]
-    transpositions = sum([len(c) - 1 for c in cycles])
-    return (-1)**transpositions
 
 
 
--- src/test_puzzle.py
+++ src/test_puzzle.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 import unittest
 import puzzle as p
+import numpy as np
 
+
+
+### test data ###
+
+# solvable
+ps1 = np.array([[ 3,  1, 14, 11],
+                [ 9, 12,  5, 13],
+                [ 4,  7,  0,  8],
+                [ 2,  6, 10, 15]])
+
+# not solvable
+pn1 = np.array([[ 3,  1, 14, 11],
+                [ 9, 12,  5, 13],
+                [ 2,  7,  0,  8],
+                [ 4,  6, 10, 15]])
+
+
+### test class(es) ###
 
 class TestStuff(unittest.TestCase):
 
@@ -11,6 +30,30 @@ class TestStuff(unittest.TestCase):
     def test_rotate(self):
         self.assertEqual(p.rotate([0, 1, 2]), [1, 2, 0])
         self.assertEqual(p.rotate([0, 1, 2], n = 2), [2, 0, 1])
+
+    def test_on_field(self):
+        self.assertTrue(p.on_field((4, 4), (3, 3)))
+        self.assertTrue(p.on_field((4, 4), (0, 0)))
+        self.assertFalse(p.on_field((4, 4), (-1, 0)))
+        self.assertFalse(p.on_field((4, 4), (4, 0)))
+        self.assertTrue(p.on_field((4, 4, 2), (0, 0, 0)))
+        self.assertFalse(p.on_field((4, 4, 2), (0, 0, 2)))
+
+    def test_reverse_action(self):
+        self.assertEqual(p.reverse_action((1, 0)), (-1, 0))
+        self.assertEqual(p.reverse_action((-1, 1)), (1, -1))
+        self.assertEqual(p.reverse_action((-1, 1, 0)), (1, -1, 0))
+
+    def test_solvable(self):
+        self.assertTrue(p.solvable(ps1))
+        self.assertFalse(p.solvable(pn1))
+
+    def equal_from_list_with_array(self, a):
+        return all(p.from_list(list(a.flat), a.shape).flat == a.flat)
+
+    def test_from_list(self):
+        self.assertTrue(self.equal_from_list_with_array(ps1))
+        self.assertTrue(self.equal_from_list_with_array(pn1))
 
 
 if __name__ == "__main__":
